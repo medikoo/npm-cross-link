bfe041cab595899633c38c16161d428151a4f1a1
Mariusz Nowak <medyk@medikoo.com>
2018-10-11 17:20:48 +0200

    feat: cli progress

--- bin/dev-package.js
+++ bin/dev-package.js
@@ -6,7 +6,7 @@ Error.stackTraceLimit = Infinity;
 
 process.on("unhandledRejection", reason => { throw reason; });
 
-require("log4-nodejs")();
+require("log4-nodejs")({ defaultNamespace: "dev-package" });
 
 const meta = require("../package")
     , argv = require("minimist")(process.argv.slice(2));
@@ -52,13 +52,52 @@ if (!packageName) {
 	process.exit(1);
 }
 
-const clc                      = require("cli-color")
+const log                      = require("log4").get("dev-package")
+    , clc                      = require("cli-color")
+    , format                   = require("cli-sprintf-format")
+    , cliFooter                = require("cli-progress-footer")()
     , DevPackageError          = require("../lib/dev-package-error")
     , installPackage           = require("../lib/install-package")
     , resolveUserConfiguration = require("../lib/resolve-user-configuration");
 
+const installsInProgress = new Set();
+
+let progressLines = ["resolving user configuration"];
+const frames = ["⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏"];
+let frameIndex = 0;
+let frame = frames[frameIndex++ % frames.length];
+
+const updateProgress = () => {
+	cliFooter.updateProgress(
+		progressLines.map(progressLine => `${ frame } ${ progressLine }`).join("\n")
+	);
+};
+const interval = setInterval(() => {
+	frame = frames[frameIndex++ % frames.length];
+	updateProgress();
+}, 80);
+const updateInstallProgressLines = () => {
+	progressLines = Array.from(installsInProgress, inProgressPackageName =>
+		format("installing %s", inProgressPackageName)
+	);
+	updateProgress();
+};
+
+installPackage.on("start", startedPackageName => {
+	installsInProgress.add(startedPackageName);
+	updateInstallProgressLines();
+});
+installPackage.on("end", endedPackageName => {
+	installsInProgress.delete(endedPackageName);
+	log.notice("installed %s", endedPackageName);
+	updateInstallProgressLines();
+});
 resolveUserConfiguration()
 	.then(configuration => installPackage(packageName, configuration))
+	.finally(() => {
+		clearInterval(interval);
+		updateProgress();
+	})
 	.catch(error => {
 		if (error instanceof DevPackageError) {
 			process.stdout.write(`\n${ clc.red(error.message) }\n`);
