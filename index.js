83766d609cbf3b19c35abb0d191375438d8a14ce
Mariusz Nowak <medyk@medikoo.com>
2018-10-26 13:29:28 +0200

    feat: drop singleton nature
    Additionally refactor setupDependencies to external module

--- index.js
+++ index.js
@@ -2,10 +2,19 @@
 
 const toPlainObject       = require("es5-ext/object/normalize-options")
     , ensureString        = require("es5-ext/object/validate-stringifiable-value")
+    , ee                  = require("event-emitter")
+    , unifyEmitters       = require("event-emitter/unify")
     , ensureConfiguration = require("./lib/ensure-configuration")
     , installPackage      = require("./lib/install-package");
 
-module.exports = (name, configuration, options = {}) =>
-	installPackage(
-		{ name: ensureString(name) }, ensureConfiguration(configuration), toPlainObject(options)
+module.exports = (name, configuration, options = {}) => {
+	const progressData = ee({ done: new Set(), ongoingMap: new Map() });
+	const promise = ee(
+		installPackage(
+			{ name: ensureString(name) }, ensureConfiguration(configuration),
+			toPlainObject(options), progressData
+		)
 	);
+	unifyEmitters(progressData, promise);
+	return promise;
+};
