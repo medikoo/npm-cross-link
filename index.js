d84adcec7ad3ffd20f44fbb6aaee91e24abdaacc
Mariusz Nowak <medyk@medikoo.com>
2018-10-26 13:52:27 +0200

    refactor: rename module

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const toPlainObject       = require("es5-ext/object/normalize-options")
     , ensureString        = require("es5-ext/object/validate-stringifiable-value")
     , ee                  = require("event-emitter")
     , unifyEmitters       = require("event-emitter/unify")
-    , ensureConfiguration = require("./lib/private/ensure-configuration")
+    , ensureConfiguration = require("./lib/private/ensure-user-configuration")
     , installPackage      = require("./lib/private/install-package/index");
 
 module.exports = (name, configuration, options = {}) => {
