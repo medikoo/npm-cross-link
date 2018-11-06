fea1cd6ed9aca1181090a73026a8de6323a92d73
Mariusz Nowak <medyk@medikoo.com>
2018-11-06 16:34:42 +0100

    refactor: simplify require path

--- install-package.js
+++ install-package.js
@@ -5,7 +5,7 @@ const toPlainObject       = require("es5-ext/object/normalize-options")
     , ee                  = require("event-emitter")
     , unifyEmitters       = require("event-emitter/unify")
     , ensureConfiguration = require("./lib/private/ensure-user-configuration")
-    , installPackage      = require("./lib/private/install-package/index");
+    , installPackage      = require("./lib/private/install-package");
 
 module.exports = (name, configuration, options = {}) => {
 	const progressData = ee({ done: new Set(), ongoingMap: new Map(), externalsMap: new Map() });
