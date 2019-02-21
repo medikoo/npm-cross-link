4b6db15e9c9fed64208da121225d00c70c7016ab
Mariusz Nowak <medyk@medikoo.com>
2019-02-21 14:23:07 +0100

    fix: do not proceed if `package.json` not found

--- install.js
+++ install.js
@@ -3,6 +3,7 @@
 const toPlainObject       = require("es5-ext/object/normalize-options")
     , ensureString        = require("es5-ext/object/validate-stringifiable-value")
     , { resolve }         = require("path")
+    , NpmCrossLinkError   = require("./lib/npm-cross-link-error")
     , ensureConfiguration = require("./lib/private/ensure-user-configuration")
     , createProgressData  = require("./lib/private/create-progress-data")
     , install             = require("./lib/private/install")
@@ -13,6 +14,9 @@ module.exports = (path, configuration, options = {}) => {
 	const progressData = createProgressData();
 	const packageContext = { path };
 	packageContext.packageJson = getPackageJson(path);
+	if (!packageContext.packageJson) {
+		return Promise.reject(new NpmCrossLinkError(`Could not find package.json at ${ path }`));
+	}
 	progressData.topPackageName = packageContext.name = packageContext.packageJson.name;
 	const promise = install(
 		packageContext, ensureConfiguration(configuration), toPlainObject(options), progressData
