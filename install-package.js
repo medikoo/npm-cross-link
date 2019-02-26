39d338018156bfe472ef7a8e5c6030a629a6f5ac
Mariusz Nowak <medyk@medikoo.com>
2019-02-26 20:32:55 +0100

    refactor: rename install-package into install-maintained-package

--- install-package.js
+++ install-package.js
@@ -1,16 +1,16 @@
 "use strict";
 
-const toPlainObject       = require("es5-ext/object/normalize-options")
-    , ensureString        = require("es5-ext/object/validate-stringifiable-value")
-    , ensureConfiguration = require("./lib/private/ensure-user-configuration")
-    , createProgressData  = require("./lib/private/create-progress-data")
-    , installPackage      = require("./lib/private/install-package");
+const toPlainObject            = require("es5-ext/object/normalize-options")
+    , ensureString             = require("es5-ext/object/validate-stringifiable-value")
+    , ensureConfiguration      = require("./lib/private/ensure-user-configuration")
+    , createProgressData       = require("./lib/private/create-progress-data")
+    , installMaintainedPackage = require("./lib/private/install-maintained-package");
 
 module.exports = (name, configuration, options = {}) => {
 	name = ensureString(name);
 	const progressData = createProgressData();
 	progressData.topPackageName = name;
-	const promise = installPackage(
+	const promise = installMaintainedPackage(
 		{ name }, ensureConfiguration(configuration), toPlainObject(options), progressData
 	);
 	promise.progressData = progressData;
