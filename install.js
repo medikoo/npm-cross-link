7016c533a8c07eb764064b89c99887fe50c633fd
Mariusz Nowak <mariusz@medikoo.com>
2019-05-27 15:57:49 +0200

    fix: recover from missing pkg.name
    Fallback to dirname if pkg.name is not provided

--- install.js
+++ install.js
@@ -1,13 +1,13 @@
 "use strict";
 
-const toPlainObject       = require("es5-ext/object/normalize-options")
-    , ensureString        = require("es5-ext/object/validate-stringifiable-value")
-    , { resolve }         = require("path")
-    , NpmCrossLinkError   = require("./lib/npm-cross-link-error")
-    , ensureConfiguration = require("./lib/private/ensure-user-configuration")
-    , createProgressData  = require("./lib/private/create-progress-data")
-    , install             = require("./lib/private/install")
-    , getPackageJson      = require("./lib/get-package-json");
+const toPlainObject         = require("es5-ext/object/normalize-options")
+    , ensureString          = require("es5-ext/object/validate-stringifiable-value")
+    , { resolve, basename } = require("path")
+    , NpmCrossLinkError     = require("./lib/npm-cross-link-error")
+    , ensureConfiguration   = require("./lib/private/ensure-user-configuration")
+    , createProgressData    = require("./lib/private/create-progress-data")
+    , install               = require("./lib/private/install")
+    , getPackageJson        = require("./lib/get-package-json");
 
 module.exports = (path, userConfiguration, inputOptions = {}) => {
 	path = resolve(ensureString(path));
@@ -17,7 +17,8 @@ module.exports = (path, userConfiguration, inputOptions = {}) => {
 	if (!packageContext.packageJson) {
 		return Promise.reject(new NpmCrossLinkError(`Could not find package.json at ${ path }`));
 	}
-	progressData.topPackageName = packageContext.name = packageContext.packageJson.name;
+	progressData.topPackageName = packageContext.name =
+		packageContext.packageJson.name || basename(path);
 	const promise = install(
 		packageContext, ensureConfiguration(userConfiguration), toPlainObject(inputOptions),
 		progressData
